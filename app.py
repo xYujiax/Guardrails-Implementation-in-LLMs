3727ba2b2f328e4b455653f8b753691004ac8136
xYujiax <scxie@ucsd.edu>
2024-12-03 07:28:48 -0800

    custom + library guardrail

--- app.py
+++ app.py
@@ -1,60 +1,133 @@
-from profanity_check import predict
-import guardrails as gd
-from guardrails.validators import Validator, register_validator 
-from typing import Dict, List
-from rich import print
-import openai
-import streamlit as st 
-from dotenv import load_dotenv
+import streamlit as st
 import os
+from dotenv import load_dotenv
+import openai
+import guardrails as gd
+from guardrails import Guard
+from guardrails.hub import ProfanityFree # requires api key 
+from rich import print
 
-load_dotenv()
+from typing import Any, Dict
+
+from guardrails.validator_base import (
+    FailResult,
+    PassResult,
+    ValidationResult,
+    Validator,
+    register_validator,
+    ErrorSpan
+)
+
+from profanity_check import predict
+
+# read and load variable from .env into this app's py env
+load_dotenv("apikey.env") 
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 def without_guardrails(text):
-    ''' # Outdated OPEN API documentation
-    response = openai.Completion.create(
-        prompt="Translate the texts to English language\n"+text,
-        engine="text-davinci-003",
-        max_tokens=2048,
-        temperature=0)
-    '''
-    # Updated documentation
-    prompt = f"Translate the following text to English\n{text}"
-    response = openai.chat.Completion.create(
-        model="gpt-3.5-turbo",  # or "gpt-4" if you're using GPT-4
+    """
+    Returns raw response from LLM without guardrails.
+
+    ARGS:
+        text(str): input prompt from the user 
+    """
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
+            {"role": "system", "content": "You are a helpful assistant. Do not censor profanity."}, 
+            {"role": "user", "content": f"Translate the following text to English: {text}"}
+            ],
         max_tokens=2048,
-        temperature=0
-    )
+        # comment out temperature=0 later in testing, so that there are answer variations
+        temperature=1, # makes model produce deterministic output # default is 1
+        )
+    return response.choices[0].message.content
 
-    result = response['choices'][0]['content']
-    return result
+def with_hub_guardrails(output): 
+    """
+    Apply premade profanity guardrail from GuardrailAi's guardrail library to LLM response.
 
+    ARGS:
+        output(str): raw output response from LLM
+    """
+    guard = Guard().use(ProfanityFree, on_fail='exception')
+    
+    try: # validate `output` and print the `validated_output` attr if successful
+        st.success(guard.validate(output).validated_output)
+    except Exception as e: # if validation fails, print error message
+        st.error(e)
+        #st.write(dir(e))
+    return
+
+def with_og_guardrails(output):
+    """
+    Apply sexy custom-made guardrails to LLM response
+
+    ARGS:
+        output (str): raw LLM output to apply guardrails on
+    """
+    guard = Guard().use(fuck)
+    if guard.validate(output).validation_passed:
+        st.success(output)
+    else:
+        st.error("I would not dare translate such profane blasphemy.")
+    return 
+ 
+@register_validator(name="is-profanity-free", data_type="string")
+class fuck(Validator):
+
+    def validate(self, value: Any, metadata: Dict) -> ValidationResult:
+        prediction = predict([value])
+        if prediction[0] == 1:
+            return FailResult(
+                error_message=f"{value} contains profanity. "
+                f"Please return profanity-free output.",
+                fix_value="",
+                error_spans=[
+                    ErrorSpan(
+                        start=0,
+                        end=len(value),
+                        reason="This text contains profanity."
+                    )
+                ]
+            )
+        return PassResult()   
 
 rail_str = """
 <rail version="0.1">
 
 <script language='python'>
+from typing import Any, Dict
+
+from guardrails.validator_base import (
+    FailResult,
+    PassResult,
+    ValidationResult,
+    Validator,
+    register_validator,
+    ErrorSpan
+)
+
+from profanity_check import predict
 
 @register_validator(name="is-profanity-free", data_type="string")
-class IsProfanityFree(Validator):
-    global predict
-    global EventDetail
-    def validate(self, key, value, schema) -> Dict:
-        text = value
+class ProfanityFree(Validator):
+    def validate(self, value: Any, metadata: Dict) -> ValidationResult:
         prediction = predict([value])
         if prediction[0] == 1:
-            raise EventDetail(
-                key,
-                value,
-                schema,
-                f"Value {value} contains profanity language",
-                "",
+            return FailResult(
+                error_message=f"{value} contains profanity. "
+                f"Please return profanity-free output.",
+                fix_value="",
+                error_spans=[
+                    ErrorSpan(
+                        start=0,
+                        end=len(value),
+                        reason="This text contains profanity."
+                    )
+                ]
             )
-        return schema
+        return PassResult()
 </script>
 
 <output>
@@ -66,48 +139,53 @@ class IsProfanityFree(Validator):
     />
 </output>
 
-
 <prompt>
-
 Translate the given statement into english language:
-
 {{statement_to_be_translated}}
-
 @complete_json_suffix
 </prompt>
 
 </rail>
 """
 
-guard = gd.Guard.from_rail_string(rail_str)
+#guard = gd.Guard.from_rail_string(rail_str)
 
+#guard = Guard().use(ProfanityFree)
+#guard = Guard().use(fuck)
 def main():
 
     st.title("Guardrails Implementation in LLMs")
 
-    text_area = st.text_area("Enter the text to be translated")
+    text_area = st.text_area("Enter the text you want to translate")
 
     if st.button("Translate"):
-        if len(text_area)>0:
-            st.info(text_area)
+        if len(text_area) > 0: # ensure that len of string is > 0
+            st.info(text_area) # just prints the information
 
-            st.warning("Translation Without Guardrails")
-
+            st.warning("Tanslated Response Without Guardrails")
             without_guardrails_result = without_guardrails(text_area)
+            
+            
+            #without_guardrails_result = 'fuck you and ur mom'
+            
             st.success(without_guardrails_result)
-
-            st.warning("Translation With Guardrails")
-
-            raw_llm_response, validated_response = guard(
-                openai.Completion.create,
-                prompt_params={"statement_to_be_translated": text_area},
-                engine="text-davinci-003",
-                max_tokens=2048,
-                temperature=0)
-
-            st.success(f"Validated Output: {validated_response}")
+            
 
 
-if __name__ == "__main__":
-    main()
+            st.warning("Translated Response With Guardrails")
+            #with_hub_guardrails(without_guardrails_result)
+            with_og_guardrails(without_guardrails_result)
+            
 
+#if __name__ == '__main__':
+main()
+##########################################
+
+"""
+    guard = Guard().use(fuck)
+    res = guard.parse(
+        llm_output=output,
+        model="model=gpt-4o-mini",
+    )
+    st.success(res.validated_output)
+"""
